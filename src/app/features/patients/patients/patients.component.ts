59bcae0d5a34825b07f8e5cd394c9fe082f40d07
DPanarin <dmitriy.panarin@logiqlabs.com>
2021-12-21 00:30:00 +0200

    added table with list of patients

--- src/app/features/patients/patients/patients.component.ts
+++ src/app/features/patients/patients/patients.component.ts
@@ -2,6 +2,9 @@ import {ChangeDetectionStrategy, Component} from "@angular/core";
 
 import {ROUTE_ANIMATIONS_ELEMENTS} from "../../../core/core.module";
 import {PatientsDataService} from "../../../core/patients/patients-data.service";
+import {BehaviorSubject} from "rxjs";
+import {Patient} from "../../../shared/models/patient.model";
+import {finalize} from "rxjs/operators";
 
 @Component({
     selector: "st-patients",
@@ -12,12 +15,23 @@ import {PatientsDataService} from "../../../core/patients/patients-data.service"
 export class PatientsComponent {
     routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;
 
+    readonly patientsList$ = new BehaviorSubject<Patient[]>([]);
+    readonly displayedColumns: string[] = ['fullName', 'birthDate', 'code', 'defaultId', 'isFavorite'];
+    readonly isLoading$ = new BehaviorSubject<boolean>(false);
+
     constructor(private readonly dataService: PatientsDataService) {
     }
 
     loadPatientsList() {
-        this.dataService.getPatients().subscribe(patientsList => {
-            console.log(patientsList);
-        })
+        this.isLoading$.next(true);
+        this.dataService.getPatients()
+            .pipe(finalize(() => this.isLoading$.next(false)))
+            .subscribe(patientsList => {
+                this.patientsList$.next(patientsList);
+            })
+    }
+
+    isFavoritePatient(patient: Patient) {
+        return false;
     }
 }
